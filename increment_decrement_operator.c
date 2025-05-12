a104c1cd4801efbdc5f36042dd4feec674e657da
Noorain-Malik <maliknoorain99@gmail.com>
2025-05-12 18:10:40 +0530

    Add files via upload

---
+++ increment_decrement_operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a =10;
+    // Increment/Decrement Operators
+    printf("Increment/Decrement Operators:\n");
+    printf("a++ = %d\n", a++);
+    printf("Now a = %d\n", a);
+    printf("++a = %d\n", ++a);
+    printf("a-- = %d\n", a--);
+    printf("Now a = %d\n", a);
+    printf("--a = %d\n\n", --a);
+    return 0;
+}
---
+++ sizeof_operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    // sizeof Operator
+    printf("sizeof Operator:\n");
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of char: %lu bytes\n\n", sizeof(char));
+    return 0;
+}
